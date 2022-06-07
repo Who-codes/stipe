d93a4fd4135b892bdd7a6eb3e3d7da2aa5a30e7e
Sulav <sulavshrestha023@gmail.com>
2022-06-07 19:15:41 +0545

    submenu location change

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "./context";
 
 export default function Submenu() {
-  const { isSubmenuOpen } = useGlobalContext();
+  const { isSubmenuOpen, location } = useGlobalContext();
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    const subMenu = container.current;
+    const { center, bottom } = location;
+
+    subMenu.style.left = `${center}px`;
+    subMenu.style.top = `${bottom}px`;
+  }, [location]);
+
   return (
-    <aside className={`submenu ${isSubmenuOpen && "show"}`}>Submenu</aside>
+    <aside className={`submenu ${isSubmenuOpen && "show"}`} ref={container}>
+      Submenu
+    </aside>
   );
 }
